144fc874688ac40d36d0ac6a7612f4e9be15377d
lresutik <lresutik@gmail.com>
2023-04-19 19:28:08 +0200

    Init commit

---
+++ src/main/java/com/uchiharaizens/learnreactspringframework/LearnReactSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.uchiharaizens.learnreactspringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnReactSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnReactSpringFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uchiharaizens/learnreactspringframework/LearnReactSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uchiharaizens.learnreactspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnReactSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
